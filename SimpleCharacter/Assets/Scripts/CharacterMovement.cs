bd82006bb679d846283d371ae02eb6a2cc809cdd
JulienBernardo2 <4gums@bell.net>
2023-02-20 09:48:37 -0500

    Full send

---
+++ SimpleCharacter/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float jumpHeight = 1f;
+    private float gravityValue = -9.81f;
+    private CharacterController controller;
+    private float walkSpeed = 5;
+    private float runSpeed = 8;
+    Animator animator;
+ 
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        // Making sure we dont have a Y velocity if we are grounded
+        // controller.isGrounded tells you if a character is grounded ( IE Touches the ground)
+        groundedPlayer = controller.isGrounded;
+
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        // Moving the character foward according to the speed
+        float speed = GetMovementSpeed();
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * speed);
+
+        // TODO add animation SetParameters for Idle/Walk/Run
+
+        // Turning the character
+         if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        // TODO
+        UpdateAnimator();
+
+         // Since there is no physics applied on character controller we have this applies to reapply gravity
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    void UpdateAnimator()
+    {
+        if (controller.velocity.magnitude == 0)
+        {
+            animator.SetFloat("CharacterSpeed", 0);
+        } 
+        else if(walkSpeed == GetMovementSpeed())
+        {
+            animator.SetFloat("CharacterSpeed", 0.5f);
+        } 
+        else if(runSpeed == GetMovementSpeed())
+        {
+            animator.SetFloat("CharacterSpeed", 1);
+        }
+    
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.SetTrigger("isRoll");
+        }
+    }
+
+    float GetMovementSpeed()
+    {
+        if (Input.GetButton("Fire3"))// Left shift
+        {
+            return runSpeed;
+        }
+        else
+        {
+            return walkSpeed;
+        }
+    }
+}
